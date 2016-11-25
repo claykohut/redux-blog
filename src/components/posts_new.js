a254c91a00e80a0bae7e71c3be54324af81e01e7
Clay Kohut <claykohut@MacBook-Pro-2.local>
2016-11-25 14:47:49 -0500

    adding validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,16 +15,25 @@ class PostsNew extends Component {
         <div className="form-group">
            <label>Title</label>
            <input type="text" className="form-control" { ...title } />
+           <div className="text-help">
+             { title.touched ? title.error : '' }
+           </div>
         </div>
 
         <div className="form-group">
            <label>Categories</label>
            <input type="text" className="form-control" { ...categories } />
+           <div className="text-help">
+             { categories.touched ? categories.error : '' }
+           </div>
         </div>
 
         <div className="form-group">
            <label>Content</label>
            <textarea type="text" className="form-control" { ...content } />
+           <div className="text-help">
+             { content.touched ? content.error : '' }
+           </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -33,10 +42,29 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title){
+    errors.title = 'Enter a title'
+  }
+
+  if(!values.categories){
+    errors.categories = 'Enter categories'
+  }
+
+  if(!values.content){
+    errors.content = 'Enter some content'
+  }
+
+  return errors;
+}
+
 // connect: first argument is mapStateToProps, 2nd is matchDispatchToProps
 // reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is matchDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
